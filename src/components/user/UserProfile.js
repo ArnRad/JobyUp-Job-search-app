df0ccd014363de30645db72abf4bcbb969fe47ad
Arnas Radzevičius <arnaas395@gmail.com>
2020-12-16 15:59:42 +0200

    Job Search Cards display

--- src/components/user/UserProfile.js
+++ src/components/user/UserProfile.js
@@ -6,6 +6,8 @@ import Button from '@material-ui/core/Button';
 
 const UserProfile = ({ handleLogout, userid }) => {
     const [userData, setData] = useState([]);
+    const [userJobSearchData, setUserJobSearchData] = useState([]);
+    const [userEmployeeSearchData, setUserEmployeeSearchData] = useState([]);
 
     useEffect(() => {
         fire.firestore().collection("users")
@@ -13,6 +15,24 @@ const UserProfile = ({ handleLogout, userid }) => {
         .get()
         .then(querySnapshot => {
             querySnapshot.docs.map(doc => {setData(doc.data()); console.log();});
+          });
+    }, []);
+
+    useEffect(() => {
+        fire.firestore().collection("jobSearch")
+        .where("user_id", "==", userid)
+        .get()
+        .then(querySnapshot => {
+            querySnapshot.docs.map(doc => {setUserJobSearchData(oldArray => [...oldArray, doc.data()])});
+          });
+    }, []);
+
+    useEffect(() => {
+        fire.firestore().collection("employeeSearch")
+        .where("user_id", "==", userid)
+        .get()
+        .then(querySnapshot => {
+            querySnapshot.docs.map(doc => {setUserEmployeeSearchData(oldArray => [...oldArray, doc.data()])});
           });
     }, []);
 
@@ -54,34 +74,99 @@ const UserProfile = ({ handleLogout, userid }) => {
             </div>
             <div className="adds-section">
                 <div className="job-section search">
-                    <h1 className="job-section-header">Looking for job</h1>
-                    <div className="job-card">
-                        <div className="job-card-name">Marketing</div>
-                        <div className="job-card-second">Loooking for good job</div>
-                    </div>
-                    <div className="job-card">
-                        <div className="job-card-name">Marketing</div>
-                        <div className="job-card-second">Loooking for good job</div>
-                    </div>
-                    <div className="job-card">
-                        <div className="job-card-name">Marketing</div>
-                        <div className="job-card-second">Loooking for good job</div>
-                    </div>
+                    <h1 className="job-section-header">Job Search</h1>
+                    {userJobSearchData.length != 0 ? (
+                        userJobSearchData.map((job, index) => (
+                        <div className="job-card-container">
+                            <div key={index} className="job-card">
+                                <div className="job-field">
+                                    <div className="job-field-description">Job Field</div>
+                                    <div className="job-field-value"> {job.job_field}</div>
+                                </div>
+                                <div className="job-field">
+                                    <div className="job-field-description">Experience</div>
+                                    <div className="job-field-value"> {job.experience}</div>
+                                </div>
+                                <div className="job-field">
+                                    <div className="job-field-description">Skills</div>
+                                    <div className="job-field-value"> {job.skills}</div>
+                                </div>
+                                <div className="job-field">
+                                    <div className="job-field-description">Location</div>
+                                    <div className="job-field-value"> {job.location}</div>
+                                </div>
+                                <div className="job-field">
+                                    <div className="job-field-description">University</div>
+                                    <div className="job-field-value"> {job.university}</div>
+                                </div>
+                                <div className="job-field">
+                                    <div className="job-field-description">School</div>
+                                    <div className="job-field-value"> {job.school}</div>
+                                </div>
+                                <div className="job-field">
+                                    <div className="job-field-description">Languages</div>
+                                    <div className="job-field-value"> {job.languages}</div>
+                                </div>
+                                <div className="job-field">
+                                    <div className="job-field-description">Hobbies</div>
+                                    <div className="job-field-value"> {job.hobbies}</div>
+                                </div>
+                            </div> 
+                            </div>
+                        ))             
+                    ) : (
+                        <div className="no-ad-section">
+                            <h3>You have no job ad yet, if you like to add one, press button</h3>
+                            <span><Button variant="contained" className="ad-section-button" size="small" color="primary">Add</Button></span>
+                        </div>
+                    )}
                 </div>
                 <div className="job-section offer">
-                    <h1 className="job-section-header">Offer a job</h1>
-                    <div className="job-card">
-                        <div className="job-card-name">Marketing</div>
-                        <div className="job-card-second">Loooking for good job</div>
-                    </div>
-                    <div className="job-card">
-                        <div className="job-card-name">Marketing</div>
-                        <div className="job-card-second">Loooking for good job</div>
-                    </div>
-                    <div className="job-card">
-                        <div className="job-card-name">Marketing</div>
-                        <div className="job-card-second">Loooking for good job</div>
-                    </div>
+                    <h1 className="job-section-header">Employee Search</h1>
+                    {userEmployeeSearchData.length != 0 ? (
+                        userEmployeeSearchData.map((job, index) => (
+                        <div className="job-card-container">
+                            <div key={index} className="job-card">
+                                <div className="job-field">
+                                    <div className="job-field-description">Duties</div>
+                                    <div className="job-field-value"> {job.duties}</div>
+                                </div>
+                                <div className="job-field">
+                                    <div className="job-field-description">Experience</div>
+                                    <div className="job-field-value"> {job.experience}</div>
+                                </div>
+                                <div className="job-field">
+                                    <div className="job-field-description">Job Field</div>
+                                    <div className="job-field-value"> {job.job_field}</div>
+                                </div>
+                                <div className="job-field">
+                                    <div className="job-field-description">Location</div>
+                                    <div className="job-field-value"> {job.location}</div>
+                                </div>
+                                <div className="job-field">
+                                    <div className="job-field-description">Position</div>
+                                    <div className="job-field-value"> {job.position}</div>
+                                </div>
+                                <div className="job-field">
+                                    <div className="job-field-description">Salary</div>
+                                    <div className="job-field-value"> {job.salary}</div>
+                                </div>
+                                <div className="job-field">
+                                    <div className="job-field-description">Skills</div>
+                                    <div className="job-field-value"> {job.skills}</div>
+                                </div>
+                            </div> 
+                            </div>
+                        ))             
+                    ) : (
+                        <div className="no-ad-section-container">
+                            <div className="no-ad-section">
+                                <h3>You have no job ad yet, if you like to add one, press button</h3>
+                                <span><Button variant="contained" className="ad-section-button" size="small" color="primary">Add</Button></span>
+                            </div>
+                        </div>
+
+                    )}
                 </div>
             </div> 
         </div>
